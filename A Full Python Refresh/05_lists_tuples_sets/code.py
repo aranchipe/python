e4b514064b8febe5d333edba12e7d855c6eca5f0
unknown <aranchipe98@gmail.com>
2023-02-06 20:49:44 -0300

    lists_tuples_sets

--- A Full Python Refresh/05_lists_tuples_sets/code.py
+++ A Full Python Refresh/05_lists_tuples_sets/code.py
@@ -1,17 +1,21 @@
 # lists
 l = ["Bob", "Rolf", "Anne"]
-l[0] = "Lucas"  # podemos fazer isso
+l[0] = "Lucas"
+l.append("Pedro")
+l.remove("Pedro")
+# __________________________________________________________________________________________
 # tuples
 # não pode modificar uma tupla, adicionar ou remover elementos
-
 t = ("Bob", "Rolf", "Anne")
-t[0] = "Lucas"  # não podemos fazer isso
-
+# t[0] = "Lucas"  # não podemos fazer isso
+# t.append("Lucas")  # não podemos fazer isso
+# __________________________________________________________________________________________
 # sets (conjuntos)
 # pode modificar o set mas não pode ter o mesmo valor duas vezes
 # o set não mantém a ordem que foi criado
-s = {"Bob", "Rolf", "Anne", "Anne"}
+s = {"Bob", "Rolf", "Anne"}
+s.add("Lucas")
 
-print(l[0])
+print(l)
 print(t)
 print(s)
